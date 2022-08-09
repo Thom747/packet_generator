372010aa2aa119b1eb0113d16a73d6f391714c62
Thom van der Steenhoven <thomvandersteenhoven@gmail.com>
2022-08-09 18:22:45 +0200

    Basic flooding

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <csignal>
+// Excluded as I can't find new setitimer in ctime
+#include <sys/time.h> // NOLINT(modernize-deprecated-headers)
+#include <cmath>
+#include <chrono>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "argparse.h"
 
-int main(int argc, char *argv[]) {
+// Amount of microseconds in a second
+#define S_TO_US (1000000)
+
+struct arguments {
+    std::string dest_ip;
+    unsigned int dest_port;
+    double packet_freq;
+    unsigned int packet_size;
+    uint8_t packet_tos;
+    unsigned int timeout;
+    bool verbose;
+};
+
+volatile bool keyboard_interrupt{false};
+unsigned int packet_num{0};
+const int sock{socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0)};
+sockaddr_in out_addr{0};
+void *msg_buffer;
+
+struct arguments parse_args(int argc, char *argv[]) {
+    // Register arguments
     argparse::ArgumentParser parser("Packet Generator");
     parser.add_description("Send UDP packets to a destination at a specific frequency.");
     parser.add_argument("dest_IP")
@@ -19,15 +49,16 @@ int main(int argc, char *argv[]) {
             .help("IP ToS code for packet, see https://www.speedguide.net/articles/quality-of-service-tos-dscp-wmm-3477")
             .scan<'u', uint8_t>();
     parser.add_argument("-t", "--timeout")
-            .help("Timeout to send packets for in seconds. If omitted or <=0, runs indefinitely.")
-            .default_value(0.0)
+            .help("Timeout to send packets for in whole seconds. If omitted or 0, runs indefinitely.")
+            .default_value((unsigned int) 0)
             .nargs(1)
-            .scan<'f', double>();
+            .scan<'u', unsigned int>();
     parser.add_argument("-v", "--verbose")
             .help("Print debugging information")
             .default_value(false)
             .implicit_value(true);
 
+    // Attempt to parse the arguments provided
     try {
         parser.parse_args(argc, argv);
     }
@@ -37,24 +68,130 @@ int main(int argc, char *argv[]) {
         std::exit(1);
     }
 
-    std::string dest_ip{parser.get("dest_IP")};
-    unsigned int dest_port{parser.get<unsigned int>("dest_port")};
-    double packet_freq{parser.get<double>("packet_freq")};
-    unsigned int packet_size{parser.get<unsigned int>("packet_size")};
-    uint8_t packet_tos{parser.get<uint8_t>("packet_tos")};
-    double timeout{parser.get<double>("-t")};
-    bool verbose{parser.get<bool>("--verbose")};
+    struct arguments res{
+            parser.get("dest_IP"),
+            parser.get<unsigned int>("dest_port"),
+            parser.get<double>("packet_freq"),
+            parser.get<unsigned int>("packet_size"),
+            parser.get<uint8_t>("packet_tos"),
+            parser.get<unsigned int>("-t"),
+            parser.get<bool>("--verbose")
+    };
 
-    if (verbose) {
-        std::cout << "Sending UDP packets to " << dest_ip << ":" << dest_port << " at " << packet_freq << "Hz."
+    if (res.verbose) {
+        std::cout << "Sending UDP packets to " << res.dest_ip << ":" << res.dest_port << " at " << res.packet_freq
+                  << "Hz."
                   << std::endl;
-        std::cout << "Packet size is " << packet_size << "B and ToS is " << (unsigned int) packet_tos << "."
+        std::cout << "Packet size is " << res.packet_size << "B and ToS is " << (unsigned int) res.packet_tos << "."
                   << std::endl;
-        if (timeout > 0)
-            std::cout << "Timeout in " << timeout << "seconds." << std::endl;
+        if (res.timeout)
+            std::cout << "Timeout in " << res.timeout << " seconds." << std::endl;
         else
             std::cout << "Timeout not specified, running indefinitely." << std::endl;
     }
 
+    return res;
+}
+
+int inline await_and_send(const struct arguments &args, sigset_t *alarm_sig, int *signum) {
+    // Wait for interrupt
+    sigwait(alarm_sig, signum);
+
+    // Send packet
+    printf("Sending packet %u\n", packet_num);
+    if (sendto(sock, msg_buffer, args.packet_size, 0, (sockaddr *) &out_addr, sizeof(out_addr)) < 0) {
+        perror("Failed to send packet");
+    }
+
+    packet_num++;
+    return 0;
+}
+
+void keyboard_interrupt_handler([[maybe_unused]] int signum) {
+    keyboard_interrupt = true;
+}
+
+void missed_alarm_handler([[maybe_unused]] int signum) {
+    write(1, "Missed alarm!\n", 14);
+}
+
+void report_stats(std::chrono::duration<double, std::micro> duration) {
+    std::cout << "Ran for " << duration.count() / S_TO_US << " seconds, sent " << packet_num << " packets."
+              << std::endl;
+}
+
+int set_and_start_timer(const struct arguments &args) {
+    long us_per_packet{(long) floor(S_TO_US / args.packet_freq)};
+    long s_per_packet{(long) us_per_packet / S_TO_US};
+    us_per_packet -= s_per_packet * S_TO_US;
+    printf("%lds, %ldus\n", s_per_packet, us_per_packet);
+
+    // Handle keyboard interrupts
+    std::signal(SIGINT, keyboard_interrupt_handler);
+    std::signal(SIGTERM, keyboard_interrupt_handler);
+
+    // Handle missed alarms
+    std::signal(SIGALRM, missed_alarm_handler);
+
+    // Register alarm
+    sigset_t alarm_sig;
+    int signum{0};
+    sigemptyset(&alarm_sig);
+    sigaddset(&alarm_sig, SIGALRM);
+
+    // Create timer: https://stackoverflow.com/questions/25327519/how-to-send-udp-packet-every-1-ms
+    struct itimerval timer{0, us_per_packet, 0, 1000};
+    if (setitimer(ITIMER_REAL, &timer, nullptr) < 0) {
+        perror("Failed to set timer");
+        exit(errno);
+    }
+
+    if (args.timeout) {
+        const std::chrono::duration<double, std::micro> timeout_duration{args.timeout * S_TO_US};
+        std::chrono::duration<double, std::micro> diff{0};
+        const auto start_time = std::chrono::high_resolution_clock::now();
+
+        do {
+            await_and_send(args, &alarm_sig, &signum);
+            diff = std::chrono::high_resolution_clock::now() - start_time;
+        } while (!keyboard_interrupt && diff < timeout_duration);
+
+        report_stats(diff);
+
+    } else {
+        const auto start_time = std::chrono::high_resolution_clock::now();
+        while (!keyboard_interrupt) {
+            await_and_send(args, &alarm_sig, &signum);
+        }
+        const auto end_time = std::chrono::high_resolution_clock::now();
+
+        report_stats(end_time - start_time);
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (sock < 0) {
+        perror("Can't open socket");
+        exit(errno);
+    }
+
+    struct arguments args{parse_args(argc, argv)};
+
+    msg_buffer = calloc(args.packet_size, sizeof(char));
+    if (msg_buffer == nullptr) {
+        perror("Can't calloc msg_buffer");
+        exit(errno);
+    }
+
+    out_addr.sin_family = AF_INET;
+    out_addr.sin_addr.s_addr = inet_addr(args.dest_ip.c_str());
+    out_addr.sin_port = htons(args.dest_port);
+
+    set_and_start_timer(args);
+
+    close(sock);
+    free(msg_buffer);
     return 0;
 }
